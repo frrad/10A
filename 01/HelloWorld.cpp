044d77c7c26b69e7a4317bf571ea3122ed837cd6
Frederick Robinson <frederick.robinson@gmail.com>
2014-04-02 23:06:23 -0700

    added hw program

---
+++ 01/HelloWorld.cpp
@@ -0,0 +1,21 @@
+/*
+Your header goes in a comment like this one.
+The comment can span multiple lines.
+*/ 
+
+//This is a one line comment.
+ 
+#include <iostream>
+
+//This is where count, endl live.
+using namespace std;
+
+//The main function is what runs when you start your program.
+int main()
+{
+  //We could use "\n" instead of endl
+  cout << "Hello World!" << endl;
+  
+  //The program returning zero means that it exited successfully
+  return 0;
+}
