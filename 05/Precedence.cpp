53e88e62b84122315ca836ad0640a12fda5f5bb8
Frederick Robinson <frederick.robinson@gmail.com>
2014-04-17 13:53:27 -0700

    before class thurs

---
+++ 05/Precedence.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int x = 1; int y = 2;
+  int z = (y = x);
+  cout << "Ex 0: " << z << endl;
+  /*
+  x = 1; y = 2;
+  x *= y + (x+=3);
+  cout << "Ex 1: " << x << endl;
+
+  x = 10;
+  (x*=2)+=2;
+  cout << "Ex 2: " << x << endl;
+
+  x = 10;
+  (x*=2)+= (x);
+  cout << "Ex 3: " << x << endl;
+
+  x = 2; y = 4;
+  z = x + ++y;
+  cout << "Ex 4: " << "x=" << x << " y=" << y <<" z=" << z << endl;
+
+  x = 2; y = 4;
+  z = x++ + y;
+  cout << "Ex 5: " << "x=" << x << " y=" << y <<" z=" << z << endl;
+
+  x = 2; y = 4;
+  z = x +++ y;
+  cout << "Ex 6: " << "x=" << x << " y=" << y <<" z=" << z << endl;
+  */
+
+  //some stuff with int / float
+}
