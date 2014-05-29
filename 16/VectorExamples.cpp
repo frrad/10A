ae1208ca25b0442c60d5a674859d16f6bf30d75a
Frederick Robinson <frederick.robinson@gmail.com>
2014-05-29 13:23:23 -0700

    srortort

--- 16/VectorExamples.cpp
+++ 16/VectorExamples.cpp
@@ -1,27 +1,66 @@
 #include <vector>
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
+
+void printVect(vector<int> &bob) {
+    for (int i = 0; i < bob.size(); i++) {
+        cout << i << " : " << bob[i] << endl;
+    }
+}
+
+void randomize(vector<int> &joe) {
+    for (int i = 0; i < joe.size(); i++) {
+        joe[i] = rand();
+    }
+}
+
+vector<int> insert(vector<int> &input, int pos, int val) {
+
+    vector<int> temp;
+    for (int i = 0; i < pos; i++) {
+        temp.push_back(input[i]);
+    }
+
+    temp.push_back(val);
+
+    for (int i = pos; i < input.size(); i++) {
+        temp.push_back(input[i]);
+    }
+
+    return temp;
+}
+
+vector<int> sort(vector<int> &unsorted) {
+    vector<int> sorted;
+
+    for (int i = 0; i < unsorted.size(); i++) {
+        // insert in correct ordoerp
+        int insertMe = unsorted[i];
+        int j = 0;
+        while (j < sorted.size() && insertMe > sorted[j]) {
+            j++;
+        }
+        sorted = insert(sorted, j, insertMe);
+        // printVect(sorted);
+    }
+
+    return sorted;
+}
 
 int main() {
     vector<int> example(10, 0);
 
-    cout << example.size() << endl;     // 10
-    cout << example.capacity() << endl; // also 10
+    example[7] = 12345;
+    cout << example[7] << endl;
 
-    example.resize(5);
-    cout << example.size() << endl;     // now it's 5
-    cout << example.capacity() << endl; // still 10
+    randomize(example);
+    printVect(example);
 
-    example.clear();
-    cout << example.size() << endl;     // and zero
-    cout << example.capacity() << endl; // still 10
+    example = sort(example);
+    printVect(example);
 
     // What does example.empty(); return at various stages?
     // example.fron() and example.back() work as you'd expect
-
-    // What about this one?
-    vector<int> ex2;
-
-    cout << ex2.size() << endl;     // 0
-    cout << ex2.capacity() << endl; // also 0
 }
