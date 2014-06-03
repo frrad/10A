8c0d88519ecc83be2757d2934eee6514e777f261
Frederick Robinson <frederick.robinson@gmail.com>
2014-06-03 09:24:28 -0700

    insertion sort with vectors

--- 16/VectorExamples.cpp
+++ 16/VectorExamples.cpp
@@ -8,6 +8,7 @@ void printVect(vector<int> &bob) {
     for (int i = 0; i < bob.size(); i++) {
         cout << i << " : " << bob[i] << endl;
     }
+    cout << "----------------" << endl;
 }
 
 void randomize(vector<int> &joe) {
@@ -16,33 +17,18 @@ void randomize(vector<int> &joe) {
     }
 }
 
-vector<int> insert(vector<int> &input, int pos, int val) {
-
-    vector<int> temp;
-    for (int i = 0; i < pos; i++) {
-        temp.push_back(input[i]);
-    }
-
-    temp.push_back(val);
-
-    for (int i = pos; i < input.size(); i++) {
-        temp.push_back(input[i]);
-    }
-
-    return temp;
-}
-
 vector<int> sort(vector<int> &unsorted) {
     vector<int> sorted;
 
     for (int i = 0; i < unsorted.size(); i++) {
-        // insert in correct ordoerp
+        // insert in correct ordoer
         int insertMe = unsorted[i];
         int j = 0;
-        while (j < sorted.size() && insertMe > sorted[j]) {
+        while (j < sorted.size() && insertMe > sorted[j])
             j++;
-        }
-        sorted = insert(sorted, j, insertMe);
+
+        sorted.insert(sorted.begin() + j, insertMe);
+
         // printVect(sorted);
     }
 
@@ -53,7 +39,7 @@ int main() {
     vector<int> example(10, 0);
 
     example[7] = 12345;
-    cout << example[7] << endl;
+    printVect(example);
 
     randomize(example);
     printVect(example);
@@ -62,5 +48,5 @@ int main() {
     printVect(example);
 
     // What does example.empty(); return at various stages?
-    // example.fron() and example.back() work as you'd expect
+    // example.front() and example.back() work as you'd expect
 }
